873d6f7ef1d46843777a81f93bba8b243d0d9f40
Aslan Imanalin <7303058+Cypher-Skif@users.noreply.github.com>
2021-07-31 18:01:05 +0300

    Added comment based help

--- Functions/Public/Get-GraphDeviceAuthToken.ps1
+++ Functions/Public/Get-GraphDeviceAuthToken.ps1
@@ -1,3 +1,28 @@
+<#
+.SYNOPSIS
+    Function to get a device authentication token.
+.DESCRIPTION
+    This function works only if your tenant satisfy the pre-requisites below:
+        - Registered Graph API application with required permissions (depends of the requests that you need)
+        - Enabled redirection for Mobile and desktop applications. More details here: https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-register-app#register-a-new-application-using-the-azure-portal
+        - Configured redirect URL: https://localhost
+        - '"allowPublicClient": true' in application Manifest json
+.EXAMPLE
+    PS C:\> Get-GraphDeviceAuthToken -TenantName 'contoso' -AppId '246c7445-eee6-4d60-968d-f83d67183753' 
+    Getting the device auth token for Contoso tenant using application ID registered in Azure AD
+.PARAMETER TenantName 
+    You can find your tenant name using Azure AD  portal > Overview > Basic information > Name
+.PARAMETER AppId
+    Фpplication ID registered in Azure AD
+.INPUTS
+    None. You cannot pipe objects to Get-GraphDeviceAuthToke
+.OUTPUTS
+    System.Array. Returns the array with token
+.LINK
+    Source code of this function: https://github.com/aslan-im/GraphApiRequests/blob/main/Functions/Public/Get-GraphDeviceAuthToken.ps1
+.LINK 
+    Source code of whole project: https://github.com/aslan-im/GraphApiRequests
+#>
 function Get-GraphDeviceAuthToken {
     [CmdletBinding()]
     param (
@@ -51,6 +76,7 @@ function Get-GraphDeviceAuthToken {
         return $TokenResponse
     }
     catch [System.Net.WebException]{
+
         if ($null -eq $_.Exception.Response){
     		throw
     	}
@@ -59,17 +85,9 @@ function Get-GraphDeviceAuthToken {
     	$Reader = New-Object System.IO.StreamReader($Result)
     	$Reader.BaseStream.Position = 0
     	$ErrorBody = ConvertFrom-Json $Reader.ReadToEnd()
-
+        
         if ($ErrorBody.Error -ne "authorization_pending"){
     		throw
     	}
-    }
-
-    If ($null -eq $TokenResponse){
-    	Write-Warning "Not Connected"
-    }
-    else{
-    	Write-Output "Connected has been established"
-    }
-    
+    }    
 }
